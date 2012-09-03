da71067ef0ac52de5c6e846ec477a4a09a7fc947
Quentin Glidic <sardemff7+git@sardemff7.net>
2012-09-03 18:14:07 +0200

    nm/ethernet: Display link speed

--- src/input/nm/nm.c
+++ src/input/nm/nm.c
@@ -23,6 +23,7 @@
 #include <glib.h>
 #include <libnm-glib/nm-client.h>
 #include <libnm-glib/nm-device.h>
+#include <libnm-glib/nm-device-ethernet.h>
 #include <libnm-glib/nm-device-wifi.h>
 
 #include <j4status-plugin.h>
@@ -170,6 +171,13 @@ _j4status_nm_device_update(J4statusPluginContext *context, J4statusSection *sect
             section->value = g_strdup_printf("%s(%s%uMb/s)", addresses->str, ( ap_text != NULL ) ? ap_text : "", bitrate/1000);
             g_free(ap_text);
             g_string_free(addresses, TRUE);
+        }
+        break;
+        case NM_DEVICE_TYPE_ETHERNET:
+        {
+            guint32 speed;
+            speed = nm_device_ethernet_get_speed(NM_DEVICE_ETHERNET(device));
+            section->value = g_strdup_printf("%s(%uMb/s)", addresses->str, speed/1000);
         }
         break;
         default:
