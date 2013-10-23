24538f88b6b83abe7b4ba27d8ae91232bb697b21
Quentin Glidic <sardemff7+git@sardemff7.net>
2013-10-23 10:32:18 +0200

    autotools: Move the xsltproc conditions to the rule
    Signed-off-by: Quentin Glidic <sardemff7+git@sardemff7.net>

--- man/man.mk
+++ man/man.mk
@@ -13,6 +13,7 @@ man/%.1 man/%.5: man/%.xml man/config.dtd
 		$(XSLTPROC) \
 		-o $(dir $@) \
 		$(AM_XSLTPROCFLAGS) \
+		--stringparam profile.condition '$(subst $(space),;,$(XSLTPROC_CONDITIONS))' \
 		http://docbook.sourceforge.net/release/xsl/current/manpages/profile-docbook.xsl \
 		$<
 
