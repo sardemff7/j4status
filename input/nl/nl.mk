cf2a318cc029daec1dab5cdca88b42c3b089560e
Quentin Glidic <sardemff7+git@sardemff7.net>
2016-05-25 11:42:23 +0200

    nl: Revert move to j4status-plugins
    Inline a few small fixes from j4status-plugins:
    - Small style fixes
    - Fix uninitialized variable
    - Fix addresses printing
    
    Signed-off-by: Quentin Glidic <sardemff7+git@sardemff7.net>

---
+++ input/nl/nl.mk
@@ -0,0 +1,37 @@
+include src/libgwater/nl.mk
+
+plugins_LTLIBRARIES += \
+	nl.la \
+	$(null)
+
+nl_la_SOURCES = \
+	input/nl/src/nl.c \
+	$(null)
+
+nl_la_CPPFLAGS = \
+	$(AM_CPPFLAGS) \
+	-D G_LOG_DOMAIN=\"j4status-nl\" \
+	$(null)
+
+nl_la_CFLAGS = \
+	$(AM_CFLAGS) \
+	$(GW_NL_CFLAGS) \
+	$(GOBJECT_CFLAGS) \
+	$(GLIB_CFLAGS) \
+	$(null)
+
+nl_la_LDFLAGS = \
+	$(AM_LDFLAGS) \
+	-module -avoid-version -export-symbols-regex j4status_input \
+	$(null)
+
+nl_la_LIBADD = \
+	libj4status-plugin.la \
+	$(GW_NL_LIBS) \
+	$(GOBJECT_LIBS) \
+	$(GLIB_LIBS) \
+	$(null)
+
+man5_MANS += \
+	input/nl/man/j4status-nl.conf.5 \
+	$(null)
